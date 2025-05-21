542bd1a5398459eeb4cadc5b04eb0d70b6bd06db
TutelerTundra76 <KeatonWilliamson7@gmail.com>
2025-05-21 14:16:51 -0500

    added textures and animations

--- scenes/player/bullet.gd
+++ scenes/player/bullet.gd
@@ -1,5 +1,5 @@
 extends RigidBody2D
 
-func _ready() -> void:
+func fire() -> void:
 	var force:=Vector2(cos(rotation),sin(rotation))*1000
 	linear_velocity=force
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,21 +1,43 @@
 extends RigidBody2D
 @export var speed:=100
 @export var deceleration:=.25
+@export var acceleration:=1.0
+@export var maxSpeed:=500.0
 @export var arm:Node2D
 @export var hand:Node2D
+@export var sprite:AnimatedSprite2D
+var direction:=1
 var bullet :=preload("res://scenes/player/bullet.tscn")
 func _physics_process(delta: float) -> void:
-	var dir:=Input.get_vector("left","right","forward","back")
-	apply_central_force(dir* speed)
+	var dir:=Input.get_vector("left","right","forward","back").normalized()
+	checkAnimations(dir)
+	linear_velocity=lerp(linear_velocity,dir*maxSpeed,acceleration)
 	
 	arm.look_at(get_global_mouse_position())
+
 	if dir==Vector2.ZERO:
 		linear_velocity=lerp(linear_velocity,Vector2.ZERO,deceleration)
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("shoot"):
+		%bow.play("shoot")
 		var bull:RigidBody2D= bullet.instantiate()
-		bull.global_position= hand.global_position
-		bull.rotation=arm.rotation
+		bull.global_transform= hand.global_transform
+		
+		
+		await %bow.animation_finished
 		get_tree().root.add_child(bull)
+		bull.fire()
 		
 		
+func checkAnimations(dir):
+	if dir.x<0 and direction==1:
+		direction=-1
+		sprite.flip_h=true
+	elif dir.x>0 and direction==-1:
+		direction=1
+		sprite.flip_h=false
+		
+	if linear_velocity.length()>20 and sprite.animation!="run":
+		sprite.play("run")
+	elif linear_velocity.length()<=20 and sprite.animation!="idle":
+		sprite.play("idle")
